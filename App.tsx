8a5c7f0c956e67637285de03758a6c894d01cf7c
Davi <dr4714@gmail.com>
2024-03-13 20:11:54 -0300

    tela de login

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container01}>
+        <Text style={styles.entrar}>Entrar</Text>
+        <TextInput placeholder='Email ou número de telefone' style={styles.login} placeholderTextColor={"#9C9C9C"}/>
+        <TextInput placeholder='Senha' style={styles.login} placeholderTextColor={"#9C9C9C"}/>
+        <TouchableOpacity style={styles.buttonentrar}>
+          <Text style={[{color:'#FFFFFF', fontSize: 15,}]}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container01: {
+    gap: 10,
+    width: "100%",
+    padding: 30,
+  },
+  entrar:{
+    color: "#FFFFFF",
+    fontSize: 15,
+  },
+  login: {
+    color: "#FFFFFF",
+    backgroundColor: "#333333",
+    padding: 10,
+    fontSize: 15,
+    borderRadius: 4,
+  },
+  buttonentrar: {
+    backgroundColor:"#E50914",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    height: 40,
+
   },
 });
